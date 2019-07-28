78e7f8f283d317abd6ed562fbc7fc906590cbf55
denishoornaert <dhoornae@ulb.ac.be>
2019-07-28 21:30:25 +0200

    Draft

--- chisel/src/main/scala/float/float.scala
+++ chisel/src/main/scala/float/float.scala
@@ -13,6 +13,66 @@ object Float {
         return res
     }
 
+    def add(Float a, Float b): Float = {
+        // SETUP
+        val res = Wire(new Float())
+        val Aidentity = identify(a)
+        val Bidentity = identify(b)
+        if((Aidentity == INF & Bidentity == NINF)|(Bidentity == INF & Aidentity == NINF)) {
+            res = 0xffc00000;
+        }
+        else if(Aidentity == INF | Aidentity == NINF) {
+            res = a;
+        }
+        else if(Bidentity == INF | Bidentity == NINF) {
+            res = b;
+        }
+        else {
+            // SHIFT EXPONENT
+            int difference = Aexponent-Bexponent;
+            unsigned Rsign = 0;
+            // if Aexp>Bexp
+            if(Aexponent > Bexponent) {
+                Bexponent = Aexponent;
+                Bmantissa = Bmantissa>>difference;
+            }
+            // if Bexp>Aexp
+            else if(Aexponent < Bexponent) {
+                Aexponent = Bexponent;
+                Amantissa = Amantissa>>absoluteValue(difference);
+            }
+            unsigned Rexponent;
+            if((toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign)) > 0) {
+                Rsign = 0;
+                Rexponent = Aexponent; // or either Bexponent;
+            }
+            else if((toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign)) < 0) {
+                Rsign = 1;
+                Rexponent = Aexponent; // or either Bexponent;
+            }
+            else {
+                Rsign = 0;
+                Rexponent = 0; // or either Bexponent;
+            }
+            // MANIPULATE
+            unsigned Rmantissa = absoluteValue(toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign));
+            if((Rmantissa&0x01000000) == 0x01000000) {
+                Rexponent = Rexponent+1;
+                if((Rmantissa&0x00000001) == 0x00000001) {
+                    Rmantissa = Rmantissa+1;
+                }
+                Rmantissa = Rmantissa>>1;
+            }
+            // TRUNCATION
+            Rsign = Rsign&0x00000001;
+            Rexponent = Rexponent&0x000000ff;
+            Rmantissa = Rmantissa&0x007fffff;
+            // CONCATENATION
+            res = (Rsign<<31)|(Rexponent<<23)|(Rmantissa);
+        }
+        return *(float*)&res;
+    }
+
 }
 
 class Float() extends Bundle {
@@ -21,64 +81,4 @@ class Float() extends Bundle {
     val exponent = UInt(8.W)
     val mantissa = UInt(23.W)
 
-    def shift32RightJamming(a: UInt, count: UInt): UInt = {
-        val res = Wire(UInt())
-        when(count === 0.U) {
-            res := a
-        }
-        .elsewhen(count < 32.U) {
-            res := (a >> count) | ((a << ((-(count.asSInt())).asUInt & 31.U)) =/= 0.U)
-        }
-        .otherwise {
-            res := (a =/= 0.U)
-        }
-        return res
-    }
-
-    def expDiffNotZero(exponent: UInt, diff: UInt, frac: UInt): UInt = {
-        val fracIntermediate = Wire(UInt())
-        val diffOut = Wire(UInt())
-        when(exponent === 0.U) {
-            diffOut := diff-1.U
-            fracIntermediate := frac
-        }
-        .otherwise {
-            diffOut := diff
-            fracIntermediate := frac | "h20000000".U
-        }
-        val fracOut = shift32RightJamming(fracIntermediate, diffOut)
-        return fracIntermediate
-    }
-
-    def endOfCondition(aFrac: UInt, bFrac: UInt, zExp: UInt): UInt = {
-        val aFracOut = aFrac | "h20000000".U
-        val zFracOut = Wire(UInt())
-        val zExpOut = Wire(UInt())
-        when(zFracOut < 0.U) {
-            zFracOut := aFrac + bFrac
-            zExpOut := zExp + 1.U
-        }
-        .otherwise {
-            zFracOut := (aFrac + bFrac) << 1.U
-            zExpOut := zExp - 1.U
-        }
-        return Cat(this.exponent, ((zExp << 23.U) + (zFracOut >> 7.U)))
-    }
-
-    def +(that: Float): Float = {
-        val res = Wire(UInt())
-        when((this.exponent-that.exponent) > 0.U) {
-            res := endOfCondition(this.mantissa, expDiffNotZero(that.exponent, this.exponent-that.exponent, that.mantissa << 6.U), this.exponent)
-        }
-        .elsewhen((this.exponent-that.exponent) < 0.U) {
-            res := endOfCondition(expDiffNotZero(this.exponent, this.exponent-that.exponent, this.mantissa << 6.U), that.mantissa, that.exponent)
-        }
-        .elsewhen(this.exponent === 0.U) {
-            res := Cat(this.exponent, 0.U(8.W), (this.mantissa+that.mantissa))
-        }
-        .otherwise {
-            res := Cat(this.exponent, (this.exponent << 23.U)+(("h40000000".U + this.mantissa + that.mantissa) >> 7.U))
-        }
-        return res.asTypeOf(new Float())
-    }
 }
