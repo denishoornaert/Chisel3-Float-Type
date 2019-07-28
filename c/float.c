3c559a749450dfb7ebabfb49d5fd508f5e78cc4a
denishoornaert <denis.hoornaert@ulb.ac.be>
2019-07-28 12:51:04 +0200

    Managemnt of the special operations (infinity and NaN)

--- c/float.c
+++ c/float.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
+#include <math.h>
 
-void split(unsigned f) {
+#define INF  0
+#define NINF 1
+#define NaN  2
+#define NNaN 3
+#define aN   4
+
+unsigned identify(unsigned f) {
     unsigned sign = f>>31;
     unsigned exponent = (f<<1)>>24;
-    unsigned mantissa = ((f<<9)>>9)|0x00800000;
-    printf("%x -> %x %x %x\n", f, sign, exponent, mantissa);
+    unsigned mantissa = ((f<<9)>>9);
+    if(exponent == 0xff) {
+        if(mantissa != 0) {
+            if(sign == 0) {
+                return NaN;
+            }
+            else {
+                return NNaN;
+            }
+        }
+        else {
+            if(sign == 0) {
+                return INF;
+            }
+            else {
+                return NINF;
+            }
+        }
+    }
+    else {
+        return aN;
+    }
 }
 
 unsigned absoluteValue(int a) {
@@ -24,83 +51,82 @@ int toInt(unsigned a, unsigned s) {
 }
 
 float add(unsigned a, unsigned b) {
+    // SETUP
+    unsigned res;
     // SPLIT
     unsigned Asign = a>>31;
     unsigned Aexponent = (a<<1)>>24;
     unsigned Amantissa = ((a<<9)>>9)|0x00800000;
-    printf("A: %x %x %x\n", Asign, Aexponent, Amantissa);
     unsigned Bsign = b>>31;
     unsigned Bexponent = (b<<1)>>24;
     unsigned Bmantissa = ((b<<9)>>9)|0x00800000;
-    printf("B: %x %x %x\n", Bsign, Bexponent, Bmantissa);
-    // SHIFT EXPONENT
-    int difference = Aexponent-Bexponent;
-    unsigned Rsign = 0;
-    // if Aexp>Bexp
-    if(Aexponent > Bexponent) {
-        Bexponent = Aexponent;
-        Bmantissa = Bmantissa>>difference;
+    unsigned Aidentity = identify(a);
+    unsigned Bidentity = identify(b);
+    if(Aidentity == NaN | Aidentity == NNaN) {
+        res = a;
     }
-    // if Bexp>Aexp
-    else if(Aexponent < Bexponent) {
-        Aexponent = Bexponent;
-        Amantissa = Amantissa>>absoluteValue(difference);
+    else if(Bidentity == NaN | Bidentity == NNaN) {
+        res = b;
     }
-    printf("toInt(%i) -> %i\n", Amantissa, toInt(Amantissa, Asign));
-    printf("toInt(%i) -> %i\n", Bmantissa, toInt(Bmantissa, Bsign));
-    unsigned Rexponent;
-    if((toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign)) > 0) {
-        printf("Asign: %u, Bsign: %u\n", Asign, Bsign);
-        printf("a > b --> %i > %i\n", toInt(Amantissa, Asign), toInt(Bmantissa, Bsign));
-        printf("%u\n", Asign);
-        Rsign = 0;
-        Rexponent = Aexponent; // or either Bexponent;
+    else if((Aidentity == INF & Bidentity == NINF)|(Bidentity == INF & Aidentity == NINF)) {
+        res = 0xffc00000;
     }
-    else if((toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign)) < 0) {
-        printf("Asign: %u, Bsign: %u\n", Asign, Bsign);
-        printf("a < b --> %i < %i\n", toInt(Amantissa, Asign), toInt(Bmantissa, Bsign));
-        printf("%u\n", Bsign);
-        Rsign = 1;
-        Rexponent = Aexponent; // or either Bexponent;
+    else if(Aidentity == INF | Aidentity == NINF) {
+        res = a;
     }
-    else {
-        Rsign = 0;
-        Rexponent = 0; // or either Bexponent;
+    else if(Bidentity == INF | Bidentity == NINF) {
+        res = b;
     }
-//    printf("A: %x %x %x\n", Asign, Aexponent, Amantissa);
-//    printf("B: %x %x %x\n", Bsign, Bexponent, Bmantissa);
-    // MANIPULATE
-    printf("%i + %i\n", toInt(Amantissa, Asign), toInt(Bmantissa, Bsign));
-    unsigned Rmantissa = absoluteValue(toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign));
-    printf("%i . %u\n", (toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign)), absoluteValue(toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign)));
-    // TRUNCATION
-//    printf("%x\n", Rsign);
-    Rsign = Rsign&0x00000001;
-//    printf("%x\n", Rexponent);
-    Rexponent = Rexponent&0x000000ff;
-//    printf("%x\n", Rmantissa);
-    Rmantissa = Rmantissa&0x007fffff;
-    // CONCATENATION
-//    printf("imp: %x %x %x\n", Rsign, Rexponent, Rmantissa);
-//  printf("Rs : %x -> %x\n", Rsign, (Rsign<<31));
-//  printf("Re : %x -> %x\n", Rexponent, (Rexponent<<23));
-    unsigned res = (Rsign<<31)|(Rexponent<<23)|(Rmantissa);
-    printf("res: %x %f\n", res, *(float*)&res);
+    else{
+        // SHIFT EXPONENT
+        int difference = Aexponent-Bexponent;
+        unsigned Rsign = 0;
+        // if Aexp>Bexp
+        if(Aexponent > Bexponent) {
+            Bexponent = Aexponent;
+            Bmantissa = Bmantissa>>difference;
+        }
+        // if Bexp>Aexp
+        else if(Aexponent < Bexponent) {
+            Aexponent = Bexponent;
+            Amantissa = Amantissa>>absoluteValue(difference);
+        }
+        unsigned Rexponent;
+        if((toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign)) > 0) {
+            Rsign = 0;
+            Rexponent = Aexponent; // or either Bexponent;
+        }
+        else if((toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign)) < 0) {
+            Rsign = 1;
+            Rexponent = Aexponent; // or either Bexponent;
+        }
+        else {
+            Rsign = 0;
+            Rexponent = 0; // or either Bexponent;
+        }
+        // MANIPULATE
+        unsigned Rmantissa = absoluteValue(toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign));
+        // TRUNCATION
+        Rsign = Rsign&0x00000001;
+        Rexponent = Rexponent&0x000000ff;
+        Rmantissa = Rmantissa&0x007fffff;
+        // CONCATENATION
+        res = (Rsign<<31)|(Rexponent<<23)|(Rmantissa);
+    }
     return *(float*)&res;
 }
 
 unsigned char assert(float a, float b) {
-    printf("-----------------------------------\n");
     unsigned char assertionResult = 0;
     float r = a+b;
     float t = add(*(unsigned*)&a, *(unsigned*)&b);
-    if(r != t) {
-        printf("Error: %f+%f should be %f but %f found.\n", a, b, r, t);
-        assertionResult = 1;
+    if((*(unsigned*)&r) == (*(unsigned*)&t)) {
+        printf("Passed: %f+%f = %f(0x%x) found.\n", a, b, t, *(unsigned*)&t);
+        assertionResult = 0;
     }
     else {
-        printf("Assertion ok !\n");
-        assertionResult = 0;
+        printf("Error: %f+%f should be %f(0x%x) but %f(0x%x) found.\n", a, b, r, *(unsigned*)&r, t, *(unsigned*)&t);
+        assertionResult = 1;
     }
     return assertionResult;
 }
@@ -119,6 +145,16 @@ int main(int argc, char const *argv[]) {
     count += assert(-3.14, 3.14);
     count += assert(0.0, 0.0);
     count += assert(-0.0, -0.0);
+    count += assert(NAN, 45.0);
+    count += assert(NAN, -45.0);
+    count += assert(-NAN, 45.0);
+    count += assert(-NAN, -45.0);
+    count += assert(INFINITY, 0.1);
+    count += assert(INFINITY, -0.1);
+    count += assert(-INFINITY, 0.1);
+    count += assert(-INFINITY, -0.1);
+    count += assert(INFINITY, INFINITY);
+    count += assert(INFINITY, -INFINITY);
     printf("%u tests failed\n", count);
     return 0;
 }
