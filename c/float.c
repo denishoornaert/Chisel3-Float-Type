d11857c4a58fbde8c0ac37be3a5182c434c1265b
denishoornaert <denis.hoornaert@ulb.ac.be>
2019-07-28 15:43:29 +0200

    Computation correction (rounding mistakes in comparison to the values generated by the FPU are still present)

--- c/float.c
+++ c/float.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #define INF  0
 #define NINF 1
-#define NaN  2
-#define NNaN 3
-#define aN   4
+#define aN   2
 
 unsigned identify(unsigned f) {
     unsigned sign = f>>31;
     unsigned exponent = (f<<1)>>24;
     unsigned mantissa = ((f<<9)>>9);
     if(exponent == 0xff) {
-        if(mantissa != 0) {
-            if(sign == 0) {
-                return NaN;
-            }
-            else {
-                return NNaN;
-            }
-        }
-        else {
+        if(mantissa == 0) {
             if(sign == 0) {
                 return INF;
             }
             else {
                 return NINF;
             }
+        }
+        else {
+            return aN;
         }
     }
     else {
@@ -62,13 +56,7 @@ float add(unsigned a, unsigned b) {
     unsigned Bmantissa = ((b<<9)>>9)|0x00800000;
     unsigned Aidentity = identify(a);
     unsigned Bidentity = identify(b);
-    if(Aidentity == NaN | Aidentity == NNaN) {
-        res = a;
-    }
-    else if(Bidentity == NaN | Bidentity == NNaN) {
-        res = b;
-    }
-    else if((Aidentity == INF & Bidentity == NINF)|(Bidentity == INF & Aidentity == NINF)) {
+    if((Aidentity == INF & Bidentity == NINF)|(Bidentity == INF & Aidentity == NINF)) {
         res = 0xffc00000;
     }
     else if(Aidentity == INF | Aidentity == NINF) {
@@ -77,7 +65,7 @@ float add(unsigned a, unsigned b) {
     else if(Bidentity == INF | Bidentity == NINF) {
         res = b;
     }
-    else{
+    else {
         // SHIFT EXPONENT
         int difference = Aexponent-Bexponent;
         unsigned Rsign = 0;
@@ -106,6 +94,13 @@ float add(unsigned a, unsigned b) {
         }
         // MANIPULATE
         unsigned Rmantissa = absoluteValue(toInt(Amantissa, Asign)+toInt(Bmantissa, Bsign));
+        if((Rmantissa&0x01000000) == 0x01000000) {
+            Rexponent = Rexponent+1;
+            if((Rmantissa&0x00000001) == 0x00000001) {
+                Rmantissa = Rmantissa+1;
+            }
+            Rmantissa = Rmantissa>>1;
+        }
         // TRUNCATION
         Rsign = Rsign&0x00000001;
         Rexponent = Rexponent&0x000000ff;
@@ -117,11 +112,11 @@ float add(unsigned a, unsigned b) {
 }
 
 unsigned char assert(float a, float b) {
+    //printf("--------------------------------------\n");
     unsigned char assertionResult = 0;
     float r = a+b;
     float t = add(*(unsigned*)&a, *(unsigned*)&b);
-    if((*(unsigned*)&r) == (*(unsigned*)&t)) {
-        printf("Passed: %f+%f = %f(0x%x) found.\n", a, b, t, *(unsigned*)&t);
+    if((r == t)||((*(unsigned*)&r) == (*(unsigned*)&t))) {
         assertionResult = 0;
     }
     else {
@@ -133,6 +128,7 @@ unsigned char assert(float a, float b) {
 
 int main(int argc, char const *argv[]) {
     unsigned char count = 0;
+    // Static tests part (corner cases)
     count += assert(2.25, 134.0625);
     count += assert(134.0625, 2.25);
     count += assert(-2.25, 134.0625);
@@ -155,6 +151,12 @@ int main(int argc, char const *argv[]) {
     count += assert(-INFINITY, -0.1);
     count += assert(INFINITY, INFINITY);
     count += assert(INFINITY, -INFINITY);
+    // Random tests
+    for (size_t i = 0; i < 100; i++) {
+        float f1 = (float)rand()/RAND_MAX;
+        float f2 = (float)rand()/RAND_MAX;
+        count += assert(f1, f2);
+    }
     printf("%u tests failed\n", count);
     return 0;
 }
