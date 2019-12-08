525fca193cfed7976e242a741006a80501f560f7
Denis Hoornaert <denis.hoornaert@outlook.com>
2019-12-08 22:59:13 +0100

    Update the FPU show case due to the modifications

--- chisel/src/main/scala/fpu/fpu.scala
+++ chisel/src/main/scala/fpu/fpu.scala
@@ -3,49 +3,43 @@ package fpu
 import chisel3._
 import chisel3.util._
 
-import float._
-import float.Float._
-import double._
-import double.Double._
+import float32._
+import float32.Float32._
+import float64._
+import float64.Float64._
 
 class FPU extends Module {
     val io = IO(new Bundle {
-        val inputType = Input(UInt(1.W)) // 0: float & double: 1
-        val operand   = Input(UInt(3.W)) // 0: mul, 1: add, 2: asUInt, 3: asSInt, 4: toDouble (UInt) & 5: toDouble (SInt)
+        val inputType = Input(UInt(1.W)) // 0: float32 & float64: 1
+        val operand   = Input(UInt(3.W)) // 0: mul, 1: add, 2: asUInt, 3: asSInt, 4: toFloat64 (UInt) & 5: toFloat64 (SInt)
         val operand1  = Input(UInt(64.W))
         val operand2  = Input(UInt(64.W))
         val result    = Output(UInt(64.W))
     })
 
     when(io.inputType.asBool) {
-        val op1 = (io.operand1).asTypeOf(new Double)
-        val op2 = (io.operand2).asTypeOf(new Double)
-
-        val res = Wire(new Double)
-        res := op1 // default value if no match
+        val res = Wire(new Float64)
+        res := io.operand1.asTypeOf(new Float64) // default value if no match
         switch(io.operand) {
-            is(0.U) { res := op1+op2 }
-            is(1.U) { res := op1*op2 }
-            is(2.U) { res := (op1.toUInt).asTypeOf(new Double) }
-            is(3.U) { res := (op1.toSInt).asTypeOf(new Double) }
-            is(4.U) { res := (op1.asUInt).toDouble }
-            is(5.U) { res := (op1.asUInt.asSInt).toDouble }
+            is(0.U) { res := (io.operand1.asTypeOf(new Float64))+(io.operand2.asTypeOf(new Float64)) }
+            is(1.U) { res := (io.operand1.asTypeOf(new Float64))*(io.operand2.asTypeOf(new Float64)) }
+            is(2.U) { res := (io.operand1.toUInt).asTypeOf(new Float64) }
+            is(3.U) { res := (io.operand1.toSInt).asTypeOf(new Float64) }
+            is(4.U) { res := (io.operand1.asUInt).toFloat64 }
+            is(5.U) { res := (io.operand1.asUInt.asSInt).toFloat64 }
         }
         io.result := res.asUInt
     }
     .otherwise {
-        val op1 = (io.operand1).asTypeOf(new Float)
-        val op2 = (io.operand2).asTypeOf(new Float)
-
-        val res = Wire(new Float)
-        res := op1 // default value if no match
+        val res = Wire(new Float32)
+        res := io.operand1.asTypeOf(new Float32) // default value if no match
         switch(io.operand) {
-            is(0.U) { res := op1+op2 }
-            is(1.U) { res := op1*op2 }
-            is(2.U) { res := (op1.toUInt).asTypeOf(new Float) }
-            is(3.U) { res := (op1.toSInt).asTypeOf(new Float) }
-            is(4.U) { res := (op1.asUInt).toFloat }
-            is(5.U) { res := (op1.asUInt.asSInt).toFloat }
+            is(0.U) { res := (io.operand1.asTypeOf(new Float32))+(io.operand2.asTypeOf(new Float32)) }
+            is(1.U) { res := (io.operand1.asTypeOf(new Float32))*(io.operand2.asTypeOf(new Float32)) }
+            is(2.U) { res := (io.operand1.toUInt).asTypeOf(new Float32) }
+            is(3.U) { res := (io.operand1.toSInt).asTypeOf(new Float32) }
+            is(4.U) { res := (io.operand1.asUInt).toFloat32 }
+            is(5.U) { res := (io.operand1.asUInt.asSInt).toFloat32 }
         }
         io.result := Cat(0.U(32.W), res.asUInt)
     }
